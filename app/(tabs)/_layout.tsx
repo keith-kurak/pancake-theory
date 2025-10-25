775c2fa7b984df222ea24f59402792cdca26e9d3
Keith Kurak <keith.kurak@gmail.com>
2025-10-25 00:24:38 -0400

    fixed android icons

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,5 +1,7 @@
-import { Icon, Label, NativeTabs } from 'expo-router/unstable-native-tabs';
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+import { Icon, Label, NativeTabs, VectorIcon } from 'expo-router/unstable-native-tabs';
 import React from 'react';
+import { Platform } from 'react-native';
 
 // TODO: fall back to JS tabs because native tabs don't look right on web
 
@@ -7,11 +9,17 @@ export default function TabLayout() {
   return (
     <NativeTabs>
       <NativeTabs.Trigger name="index">
-        <Icon sf="slider.horizontal.3" />
+        {Platform.select({
+          ios: <Icon sf="slider.horizontal.3" />,
+          android: <Icon src={<VectorIcon family={MaterialIcons} name="tune" />} />,
+        })}
         <Label hidden>Chooser</Label>
       </NativeTabs.Trigger>
       <NativeTabs.Trigger name="history">
-        <Icon sf="clock.fill" />
+        {Platform.select({
+          ios: <Icon sf="clock.fill" />,
+          android: <Icon src={<VectorIcon family={MaterialIcons} name="schedule" />} />,
+        })}
         <Label hidden>History</Label>
       </NativeTabs.Trigger>
     </NativeTabs>
