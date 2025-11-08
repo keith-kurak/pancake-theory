ed0eb9788d81cb52e8b4601885c6846ec35f8521
Keith Kurak <keith.kurak@gmail.com>
2025-11-08 00:29:54 -0500

    OTA update overlay

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -8,6 +8,7 @@ import * as SplashScreen from "expo-splash-screen";
 import { StatusBar } from "expo-status-bar";
 import "react-native-reanimated";
 
+import OtaUpdateOverlay from "@/components/OtaUpdateOverlay";
 import { useColorScheme } from "@/hooks/use-color-scheme";
 
 export const unstable_settings = {
@@ -38,6 +39,7 @@ export default function RootLayout() {
         />
       </Stack>
       <StatusBar style="auto" />
+      <OtaUpdateOverlay />
     </ThemeProvider>
   );
 }
