a9dfb9af83446ea34394ca6d5310bbebc8e34195
Keith Kurak <keith.kurak@gmail.com>
2026-02-25 13:25:05 -0500

    commit

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,7 +1,7 @@
-import MaterialIcons from '@expo/vector-icons/MaterialIcons';
-import { NativeTabs } from 'expo-router/unstable-native-tabs';
-import React from 'react';
-import { Platform } from 'react-native';
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
+import { NativeTabs } from "expo-router/unstable-native-tabs";
+import React from "react";
+import { Platform } from "react-native";
 
 // TODO: fall back to JS tabs because native tabs don't look right on web
 
@@ -11,24 +11,53 @@ export default function TabLayout() {
       <NativeTabs.Trigger name="index">
         {Platform.select({
           ios: <NativeTabs.Trigger.Icon sf="slider.horizontal.3" />,
-          android: <NativeTabs.Trigger.Icon src={<NativeTabs.Trigger.VectorIcon family={MaterialIcons} name="tune" />} />,
+          android: (
+            <NativeTabs.Trigger.Icon
+              src={
+                <NativeTabs.Trigger.VectorIcon
+                  family={MaterialIcons}
+                  name="tune"
+                />
+              }
+            />
+          ),
         })}
         <NativeTabs.Trigger.Label hidden>Chooser</NativeTabs.Trigger.Label>
       </NativeTabs.Trigger>
       <NativeTabs.Trigger name="browse">
         {Platform.select({
           ios: <NativeTabs.Trigger.Icon sf="list.bullet" />,
-          android: <NativeTabs.Trigger.Icon src={<NativeTabs.Trigger.VectorIcon family={MaterialIcons} name="view-list" />} />,
+          android: (
+            <NativeTabs.Trigger.Icon
+              src={
+                <NativeTabs.Trigger.VectorIcon
+                  family={MaterialIcons}
+                  name="view-list"
+                />
+              }
+            />
+          ),
         })}
         <NativeTabs.Trigger.Label hidden>Browse</NativeTabs.Trigger.Label>
       </NativeTabs.Trigger>
       <NativeTabs.Trigger name="history">
         {Platform.select({
           ios: <NativeTabs.Trigger.Icon sf="clock.fill" />,
-          android: <NativeTabs.Trigger.Icon src={<NativeTabs.Trigger.VectorIcon family={MaterialIcons} name="schedule" />} />,
+          android: (
+            <NativeTabs.Trigger.Icon
+              src={
+                <NativeTabs.Trigger.VectorIcon
+                  family={MaterialIcons}
+                  name="schedule"
+                />
+              }
+            />
+          ),
         })}
         <NativeTabs.Trigger.Label hidden>History</NativeTabs.Trigger.Label>
       </NativeTabs.Trigger>
     </NativeTabs>
   );
 }
+
+// comment
