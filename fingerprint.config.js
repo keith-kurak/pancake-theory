e5b03e99c7192dec1b0de1704169fa9d8c4b8c0c
Keith Kurak <keith.kurak@gmail.com>
2026-03-23 09:56:25 -0400

    update package versions

--- fingerprint.config.js
+++ fingerprint.config.js
@@ -1,11 +1,11 @@
 /** @type {import('@expo/fingerprint').Config} */
 const config = {
   sourceSkips: [
-    'ExpoConfigRuntimeVersionIfString',
-    'ExpoConfigVersions',
-    'ExpoConfigNames',
-    'PackageJsonAndroidAndIosScriptsIfNotContainRun',
-    'ExpoConfigExtraSection',
+    "ExpoConfigRuntimeVersionIfString",
+    "ExpoConfigVersions",
+    "ExpoConfigNames",
+    "PackageJsonAndroidAndIosScriptsIfNotContainRun",
+    "ExpoConfigExtraSection",
   ],
 };
 module.exports = config;
