b76759a021f37bb3884f5c57c39e77f53e995eed
Keith Kurak <keith.kurak@gmail.com>
2026-05-14 15:02:23 -0400

    test

--- metro.config.js
+++ metro.config.js
@@ -1,20 +1,20 @@
-const {
-  getSentryExpoConfig
-} = require("@sentry/react-native/metro");
+const { getSentryExpoConfig } = require("@sentry/react-native/metro");
 
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getSentryExpoConfig(__dirname);
 
 // Add wasm asset support
-config.resolver.assetExts.push('wasm');
- 
+config.resolver.assetExts.push("wasm");
+
 // Add COEP and COOP headers to support SharedArrayBuffer
 config.server.enhanceMiddleware = (middleware) => {
   return (req, res, next) => {
-    res.setHeader('Cross-Origin-Embedder-Policy', 'credentialless');
-    res.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
+    res.setHeader("Cross-Origin-Embedder-Policy", "credentialless");
+    res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
     middleware(req, res, next);
   };
 };
+
+//test
 
 module.exports = config;
