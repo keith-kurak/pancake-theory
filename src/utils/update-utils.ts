762b3eb2e287b0ddc6f1232308b6876576cc0f89
Keith Kurak <keith.kurak@gmail.com>
2026-05-18 16:00:11 -0400

    build cache provider, etc

--- src/utils/update-utils.ts
+++ src/utils/update-utils.ts
@@ -1,39 +1,45 @@
-import Constants from 'expo-constants';
-import type { CurrentlyRunningInfo, UseUpdatesReturnType } from 'expo-updates';
-import { ExpoConfig, ExpoUpdatesManifest } from 'expo/config';
+import Constants from "expo-constants";
+import type { CurrentlyRunningInfo, UseUpdatesReturnType } from "expo-updates";
+import { ExpoConfig, ExpoUpdatesManifest } from "expo/config";
 
-const expoConfig: ExpoConfig = require('../app.json').expo as unknown as ExpoConfig;
+const expoConfig: ExpoConfig = require("../app.json")
+  .expo as unknown as ExpoConfig;
 
-export const updateUrl: string = expoConfig.updates?.url ?? '';
+export const updateUrl: string = expoConfig.updates?.url ?? "";
 
 const isAvailableUpdateCritical = (updatesSystem: UseUpdatesReturnType) => {
   const { currentlyRunning, availableUpdate } = updatesSystem;
   const criticalIndexCurrent =
-    (currentlyRunning.manifest as ExpoUpdatesManifest)?.extra?.expoClient?.extra?.criticalIndex ??
+    (currentlyRunning.manifest as ExpoUpdatesManifest)?.extra?.expoClient?.extra
+      ?.criticalIndex ??
     Constants?.expoConfig?.extra?.criticalIndex ??
     0;
 
   const criticalIndexUpdate =
-    (availableUpdate?.manifest as ExpoUpdatesManifest)?.extra?.expoClient?.extra?.criticalIndex ??
-    0;
+    (availableUpdate?.manifest as ExpoUpdatesManifest)?.extra?.expoClient?.extra
+      ?.criticalIndex ?? 0;
 
   return criticalIndexUpdate > criticalIndexCurrent;
 };
 
-const isAvailableUpdateCritical_nonhook = (availableUpdate: ExpoUpdatesManifest) => {
+//test
+const isAvailableUpdateCritical_nonhook = (
+  availableUpdate: ExpoUpdatesManifest,
+) => {
   const currentManifest = Constants.expoConfig;
   const criticalIndexCurrent =
     currentManifest?.extra?.expoClient?.extra?.criticalIndex ??
     Constants?.expoConfig?.extra?.criticalIndex ??
     0;
 
-  const criticalIndexUpdate = availableUpdate.extra?.expoClient?.extra?.criticalIndex ?? 0;
+  const criticalIndexUpdate =
+    availableUpdate.extra?.expoClient?.extra?.criticalIndex ?? 0;
 
   return criticalIndexUpdate > criticalIndexCurrent;
 };
 
 const manifestMessage = (manifest: any) => {
-  return manifest?.extra?.expoClient?.extra?.message ?? '';
+  return manifest?.extra?.expoClient?.extra?.message ?? "";
 };
 
 // Utils for constructing display text
@@ -44,14 +50,16 @@ const isInDevelopmentMode = (currentlyRunning: CurrentlyRunningInfo) => {
 
 const currentlyRunningTitle = (currentlyRunning: CurrentlyRunningInfo) => {
   if (isInDevelopmentMode(currentlyRunning)) {
-    return 'No update soup for you in dev mode';
+    return "No update soup for you in dev mode";
   }
-  return currentlyRunning?.isEmbeddedLaunch ? 'Running the embedded bundle:' : 'Running an update:';
+  return currentlyRunning?.isEmbeddedLaunch
+    ? "Running the embedded bundle:"
+    : "Running an update:";
 };
 
 const currentlyRunningDescription = (
   currentlyRunning: CurrentlyRunningInfo,
-  lastCheckForUpdateTime?: Date
+  lastCheckForUpdateTime?: Date,
 ) => {
   return (
     ` ID: ${currentlyRunning.updateId}\n` +
@@ -65,26 +73,26 @@ const currentlyRunningDescription = (
 
 const availableUpdateTitle = (updatesSystem: UseUpdatesReturnType) => {
   if (isInDevelopmentMode(updatesSystem.currentlyRunning)) {
-    return 'No update soup for you in dev mode';
+    return "No update soup for you in dev mode";
   }
   return updatesSystem.isUpdateAvailable
-    ? `${isAvailableUpdateCritical(updatesSystem) ? 'A critical update' : 'An update'} ${
-        updatesSystem.isUpdatePending ? 'has been downloaded' : 'is available'
+    ? `${isAvailableUpdateCritical(updatesSystem) ? "A critical update" : "An update"} ${
+        updatesSystem.isUpdatePending ? "has been downloaded" : "is available"
       }`
-    : 'App is running the latest update';
+    : "App is running the latest update";
 };
 
 const availableUpdateDescription = (updatesSystem: UseUpdatesReturnType) => {
   if (isInDevelopmentMode(updatesSystem.currentlyRunning)) {
-    return '';
+    return "";
   }
   const availableUpdate = updatesSystem.availableUpdate;
   const updateDescription = availableUpdate
     ? ` ID: ${availableUpdate.updateId}\n` +
-      ` Created: ${availableUpdate.createdAt?.toISOString() || ''}\n` +
+      ` Created: ${availableUpdate.createdAt?.toISOString() || ""}\n` +
       ` Message: ${manifestMessage(availableUpdate.manifest)}\n` +
       ` Critical: ${isAvailableUpdateCritical(updatesSystem)}\n`
-    : 'No available update\n';
+    : "No available update\n";
   return updateDescription;
 };
 
@@ -92,15 +100,20 @@ const errorDescription = (updatesSystem: UseUpdatesReturnType) => {
   const { checkError, downloadError } = updatesSystem;
   const checkErrorDescription = checkError?.message
     ? `Error on check: ${checkError?.message}\n`
-    : '';
+    : "";
   const downloadErrorDescription = downloadError?.message
     ? `Error on download: ${downloadError?.message}\n`
-    : '';
+    : "";
   return checkErrorDescription + downloadErrorDescription;
 };
 
 export {
-  availableUpdateDescription, availableUpdateTitle, currentlyRunningDescription, currentlyRunningTitle, errorDescription, isAvailableUpdateCritical,
+  availableUpdateDescription,
+  availableUpdateTitle,
+  currentlyRunningDescription,
+  currentlyRunningTitle,
+  errorDescription,
+  isAvailableUpdateCritical,
   isAvailableUpdateCritical_nonhook,
   manifestMessage
 };
